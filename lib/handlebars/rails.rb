b1e88d7d818d33607c542fd31644f43d072beed5
Niklas Hofer <niklas+dev@lanpartei.de>
2011-07-03 22:44:18 +0200

    create an engine so the vendor directory gets recognized by the asset pipeline

--- lib/handlebars/rails.rb
+++ lib/handlebars/rails.rb
@@ -6,4 +6,7 @@ end
 require 'handlebars/tilt/handlebars_template'
 require 'handlebars/rails/template_handlers'
 require 'handlebars/rails/version'
+unless ::Rails.version < "3.1"
+  require 'handlebars/rails/engine'
+end
 
---
+++ lib/handlebars/rails/engine.rb
@@ -0,0 +1,9 @@
+module Handlebars
+  module Rails
+
+    class Engine < ::Rails::Engine
+     #  create an engine so the vendor directory gets recognized by the asset pipeline
+    end
+
+  end
+end
